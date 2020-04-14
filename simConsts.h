17cf0990ac9678e4320f9f0fe5fbfa050a1657df
Jack Coker <jfc20@ic.ac.uk>
2020-04-14 16:56:17 +0100

    comment changes

--- simConsts.h
+++ simConsts.h
@@ -1,9 +1,9 @@
 #pragma once
 
 // Physical constansts
-const double hbar = 6.58211899e-16; // eV
+const double hbar = 6.58211899e-16; // eV, reduced plank constant
 const double pi = 3.14159265; // dimensionless
-const double e = 1.60217646e-19; // C
+const double e = 1.60217646e-19; // C, elementary charge
 
 // Simulation parameters
 const double kBT = 0.02585; // eV, for T=300K

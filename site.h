0242ac29cc150211d575745b1f2d4aa3426aad28
Jack Coker <jfc20@ic.ac.uk>
2020-04-30 19:12:14 +0100

    Had to move the definition (but not declaration) of struct neighbour to after the Rate function was declared, so that it could be made friend.

--- site.h
+++ site.h
@@ -6,15 +6,10 @@ class site
 private:
 
 	// A structure that will be used to hold the position of the site.
-	struct vec { double X, Y, Z;  };
+	struct vec { double X, Y, Z; };
 
 	// A structure that will be used to specify which other sites this site interacts with, and the strength of this interaction.
-	struct neighbour {
-		site* _pSite = NULL; 
-		double _J = 0.0;
-	// private: // ToDo: Make sure only the function Rate() has access to the member below.
-		double _rate = -1.0; // -1 for not yet set, the rate will be updated the first time it is needed.
-	};
+	struct neighbour;
 
 	// The list of neighbors this site interacts with.
 	std::vector<neighbour*> neighbours;
@@ -63,6 +58,17 @@ public:
 	~site();
 
 	friend std::ostream& operator<<(std::ostream& os, const site& st);
+
+private:
+
+	struct neighbour 
+	{
+		friend double site::Rate(site* pSite, double fieldZ, double kBT, double reorg);
+		site* _pSite = NULL;
+		double _J = 0.0;
+	private: // Make sure only the function Rate() has access to the member below.
+		double _rate = -1.0; // -1 for not yet set, the rate will be updated the first time it is needed.
+	};
 
 };
 
